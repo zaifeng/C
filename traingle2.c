d344c67b982da0e92e08182615e3de3b323da610
zaifeng <20012382@qq.com>
2015-07-14 13:41:27 +0800

    new traingle pg

---
+++ traingle2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l ;
+    printf("Please input a number:");
+    scanf("%d" , &i);
+    for(j=1;j<=i;j++)
+    {
+        for(l=i-j;l>0;l--){
+            putchar(' ');
+        }
+        for(k=j;k>0;k--) {
+            putchar('*');
+            putchar(' ');
+        }
+        putchar('\n');
+    }
+    return 0 ;
+}
