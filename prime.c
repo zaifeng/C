646aee47a684594ea066f61d27620b7a88a0378c
zaifeng <20012382@qq.com>
2015-07-15 22:44:30 +0800

    prime

---
+++ prime.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX_NUM 1000
+int main()
+{
+    int prime[MAX_NUM];
+    int i = 0 ;
+    int j ,k;
+    prime[i++] = 2 ;
+    for(j=3;j<MAX_NUM;j+=2)
+    {
+        for
+    }
+    return 0 ;
+}
