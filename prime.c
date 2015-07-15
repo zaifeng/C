2223465ac0c162b65b80359d2300dbac7abc6aee
zaifeng <20012382@qq.com>
2015-07-15 23:17:04 +0800

    self prime

--- prime.c
+++ prime.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 
-#define MAX_NUM 1000
 int main()
 {
-    int prime[MAX_NUM];
-    int i = 0 ;
-    int j ,k;
-    prime[i++] = 2 ;
-    for(j=3;j<MAX_NUM;j+=2)
+    int prime[200];
+    int i , j,flag ;
+    int idx = 0 ;
+
+    prime[idx++] = 2 ;
+    prime[idx++] = 3 ;
+    for(i = 5 ; i < 1000 ;i+=2)
     {
-        for
+        flag = 0 ;
+        for(j=1 ; prime[j]*prime[j]<=i ; j++)
+        {
+            if(i% prime[j] == 0){
+                flag = 1 ;
+                break ;
+            }
+        }
+        if(!flag)
+        {
+            prime[idx++] = i ;
+        }
+    }
+    
+    for(i=0 ; i<idx ; i++)
+    {
+        printf("prime[%3d] = %d\n" , i , prime[i]);
     }
     return 0 ;
 }
